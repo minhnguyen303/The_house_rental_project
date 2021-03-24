f43b7f483f91a80c19ea7e0192e795d5880b471c
minhnguyen303 <minh09az09@gmail.com>
2021-03-24 10:59:58 +0700

    Create master layout, core/nav, core/footer

---
+++ resources/views/layout/core/footer.blade.php
@@ -0,0 +1,86 @@
+<footer id="footer" class="bg-light footer-light">
+    <div class="container container-1000">
+        <div class="row">
+            <div class="col-lg-3">
+                <p><span class="icon-uilove icon-uilove-realestate"></span></p>
+                <address class="mb-3">
+                    <strong>Twitter, Inc.</strong><br>
+                    1355 Market Street, Suite 900<br>
+                    San Francisco, CA 94103<br>
+                    <abbr title="Phone">P:</abbr> (123) 456-7890
+                </address>
+                <div class="footer-social mb-4"><a href="http://uilove.in/realestate/listo/preview/signin.html#"
+                                                   class="ml-2 mr-2"><span class="fa fa-twitter"></span></a> <a
+                        href="http://uilove.in/realestate/listo/preview/signin.html#" class="ml-2 mr-2"><span
+                            class="fa fa-facebook"></span></a> <a
+                        href="http://uilove.in/realestate/listo/preview/signin.html#" class="ml-2 mr-2"><span
+                            class="fa fa-instagram"></span></a></div>
+            </div>
+            <div class="col-lg-2 col-sm-4">
+                <div class="footer-links">
+                    <ul class="list-unstyled">
+                        <li class="list-title"><a href="http://uilove.in/realestate/listo/preview/signin.html#">About</a>
+                        </li>
+                        <li><a href="http://uilove.in/realestate/listo/preview/signin.html#">Company</a></li>
+                        <li><a href="http://uilove.in/realestate/listo/preview/signin.html#">Community</a></li>
+                        <li><a href="http://uilove.in/realestate/listo/preview/signin.html#">Carrers</a></li>
+                        <li><a href="http://uilove.in/realestate/listo/preview/signin.html#">Press</a></li>
+                        <li><a href="http://uilove.in/realestate/listo/preview/signin.html#">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-2 col-sm-4">
+                <div class="footer-links">
+                    <ul class="list-unstyled">
+                        <li class="list-title"><a href="http://uilove.in/realestate/listo/preview/signin.html#">Links</a>
+                        </li>
+                        <li><a href="http://uilove.in/realestate/listo/preview/signin.html#">For Rent</a></li>
+                        <li><a href="http://uilove.in/realestate/listo/preview/signin.html#">For Sale</a></li>
+                        <li><a href="http://uilove.in/realestate/listo/preview/signin.html#">Commercial</a></li>
+                        <li><a href="http://uilove.in/realestate/listo/preview/signin.html#">Agents</a></li>
+                        <li><a href="http://uilove.in/realestate/listo/preview/signin.html#">Property Guides</a>
+                        </li>
+                        <li><a href="http://uilove.in/realestate/listo/preview/signin.html#">Jobs</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-2 col-sm-4">
+                <div class="footer-links">
+                    <ul class="list-unstyled">
+                        <li class="list-title"><a href="http://uilove.in/realestate/listo/preview/signin.html#">Help</a>
+                        </li>
+                        <li><a href="http://uilove.in/realestate/listo/preview/signin.html#">Payments</a></li>
+                        <li><a href="http://uilove.in/realestate/listo/preview/signin.html#">Shipping</a></li>
+                        <li><a href="http://uilove.in/realestate/listo/preview/signin.html#">Cancellation</a>
+                        </li>
+                        <li><a href="http://uilove.in/realestate/listo/preview/signin.html#">FAQ</a></li>
+                        <li><a href="http://uilove.in/realestate/listo/preview/signin.html#">Report</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="text-lg-right ml-lg-2">
+                    <form>
+                        <div class="list-title">Subscribe</div>
+                        <div class="input-group input-group-lg">
+                            <input type="text" name="email" class="form-control form-control-lg subscribe-input"
+                                   placeholder="Email">
+                            <div class="input-group-append ml-0">
+                                <button class="btn subscribe-button" type="button"><i
+                                        class="fa fa-envelope"></i></button>
+                            </div>
+                        </div>
+                    </form>
+                    <div class="footer-payments"><span class="fa fa-cc-visa"></span> <span
+                            class="fa fa-cc-mastercard"></span> <span class="fa fa-cc-amex"></span></div>
+                </div>
+            </div>
+
+        </div>
+        <div class="footer-credits d-lg-flex justify-content-lg-between align-items-center">
+            <div>Powered by <strong>uilove</strong></div>
+
+            <div>© 2018 UILOVE Inc. All Rights Reserved</div>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Real Estate</title>
+
+    <!-- Bootstrap -->
+    <link href="https://fonts.googleapis.com/css?family=Libre+Franklin:100,200,300,400,500,700" rel="stylesheet">
+
+    <link href="{{asset('lib/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('lib/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet">
+    <link href="{{asset('lib/animate.css')}}" rel="stylesheet">
+    <link href="{{asset('lib/selectric/selectric.css')}}" rel="stylesheet">
+    <link href="{{asset('lib/aos/aos.css')}}" rel="stylesheet">
+    <link href="{{asset('lib/Magnific-Popup/magnific-popup.css')}}" rel="stylesheet">
+    <link href="{{asset('css/style.css')}}" rel="stylesheet">
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="{{asset('lib/jquery-3.2.1.min.js')}}"></script>
+    <script src="{{asset('lib/popper.min.js')}}"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="{{asset('lib/bootstrap/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('lib/selectric/jquery.selectric.js')}}"></script>
+    <script src="{{asset('lib/aos/aos.js')}}"></script>
+    <script src="{{asset('lib/Magnific-Popup/jquery.magnific-popup.min.js')}}"></script>
+    <script src="{{asset('lib/sticky-sidebar/ResizeSensor.min.js')}}"></script>
+    <script src="{{asset('lib/sticky-sidebar/theia-sticky-sidebar.min.js')}}"></script>
+    <script src="{{asset('lib/lib.js')}}"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <style id="theia-sticky-sidebar-stylesheet-TSS">.theiaStickySidebar:after {
+            content: "";
+            display: table;
+            clear: both;
+        }</style>
+</head>
+
+@yield('body')
+
+</html>
