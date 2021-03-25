3955ec7553973eedeed995687de16e1483dc4149
nguyenduyan1311 <annguyenazn1311@gmail.com>
2021-03-25 10:03:11 +0700

    Fix Validate Register

--- app/Http/Requests/RegisterUserRequest.php
+++ app/Http/Requests/RegisterUserRequest.php
@@ -24,7 +24,9 @@ class RegisterUserRequest extends FormRequest
             'username' => ['required', 'unique:users'],
             'email' => ['required', 'email', 'unique:users'],
             'password' => ['required', 'min:6', 'max:8', 'confirmed'],
+            'password_confirmation' => ['required', 'min:6', 'max:8'],
             'phone' => ['required', 'digits:10', 'unique:users'],
+            'terms' => ['required'],
         ];
     }
     public function messages()
@@ -33,15 +35,16 @@ class RegisterUserRequest extends FormRequest
             'username.required' => 'Tên người dùng không được để trống',
             'username.unique' => 'Tên người dùng đã tồn tại',
             'email.required' => 'Email không được để trống',
-            'email.email' => 'vui lòng nhập email đúng định dạng',
+            'email.email' => 'Email không hợp lệ',
             'email.unique' => 'Email này đã tồn tại',
-            'password.required' => 'Mật khẩu không được để trống',
-            'password.min' => 'Mật khẩu phải có ít nhất 6 ký tự',
-            'password.max' => 'Mật khẩu phải ít hơn 9 ký tự',
+            'password_confirmation.required' => 'Mật khẩu không được để trống',
+            'password_confirmation.min' => 'Mật khẩu phải có ít nhất 6 ký tự',
+            'password_confirmation.max' => 'Mật khẩu phải ít hơn 9 ký tự',
             'password.confirmed' => 'Vui lòng nhập mật khẩu khớp với nội dung phía trên',
             'phone.required' => 'Số điện thoại không được để trống',
             'phone.digits' => 'Số điện thoại không hợp lệ',
             'phone.unique' => 'Số điện thoại này đã tồn tại',
+            'terms.required' => 'Bạn phải đồng ý với điều khoản của chúng tôi'
         ];
     }
 }
